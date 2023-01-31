c03b8dbbd6985ffa808eb1dfd8a4354655f17d8c
MarekMatyas <matyasamarek@gmail.com>
2023-01-31 15:58:27 -0800

    gross_pay_task

---
+++ gross_pay.py
@@ -0,0 +1,12 @@
+hours_worked = int(input("Please enter your hours for this week."))
+hourly_rate = int(input("Thank you, now enter your hourly rate."))
+
+
+
+if hours_worked > 40:
+    overtime= hours_worked -40
+    gross_pay = (40 * hourly_rate) + (overtime * hourly_rate * 1.5)
+else:
+    gross_pay = hours_worked * hourly_rate
+print(gross_pay)
+
---
+++ hours_worked.py
@@ -0,0 +1,8 @@
+hours_worked = input("Please enter your hours for this week.")
+hourly_rate = input("Please enter your hourly rate.")
+
+
+
+
+
+
