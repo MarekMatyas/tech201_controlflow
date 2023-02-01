70bd9d5a454b9d688580bdbc0a1826861373b58a
MarekMatyas <matyasamarek@gmail.com>
2023-01-31 17:24:26 -0800

    fizz_buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,9 @@
+for fizz_buzz_number in range(1, 101):
+    if fizz_buzz_number % 3 == 0 and fizz_buzz_number % 5 == 0:
+        print("FizzBuzz")
+    elif fizz_buzz_number % 3 == 0:
+        print("Fizz")
+    elif fizz_buzz_number % 5 == 0:
+        print("Buzz")
+    else:
+        print(fizz_buzz_number)
