8edfbbf02f79cd87ad2fd5f7306c5c922895666b
MarekMatyas <matyasamarek@gmail.com>
2023-02-01 16:32:43 -0800

    movie_ratingV2

---
+++ fizz_buzzV2.py
@@ -0,0 +1,11 @@
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+for i in range(1,101):
+    print(fizz_buzz(i))
